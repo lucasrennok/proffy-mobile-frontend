e12ad8314cb1e670df807df3f8361fbf03f90a39
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-08-07 17:52:44 -0300

    teacher list

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,66 @@
+import {View, Image, Text} from 'react-native';
+import React from 'react';
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
+import whatsappIcon from '../../assets/images/icons/whatsapp.png'
+import styles from './styles';
+import { RectButton } from 'react-native-gesture-handler';
+
+export interface Teacher{
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.profile}>
+                <Image 
+                    style={styles.avatar}
+                    source={{uri: teacher.avatar}}
+                >
+                </Image>
+                <View style={styles.profileInfo}>
+                    <Text style={styles.name}>{teacher.name}</Text>
+                    <Text style={styles.subject}>{teacher.subject}</Text>
+                </View>
+            </View>
+
+            <Text style={styles.bio}>
+                {teacher.bio}
+            </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/Hora {'   '}
+                <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={[styles.favoriteButton, styles.favorited]}>
+                        {/* <Image source={heartOutlineIcon}></Image> */}
+                        <Image source={unfavoriteIcon}></Image>
+                    </RectButton>
+
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon}></Image>
+                        <Text style={styles.contactButtonText}>
+                            Entrar em contato
+                        </Text>
+                    </RectButton>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+export default TeacherItem;
